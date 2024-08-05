e4b395a9d1ea21af68852272ac58b1c6967af3d5
Timothy Pidashev <pidashev.tim@gmail.com>
2024-08-04 19:08:07 -0700

    Update time component

--- components/datetime.c
+++ components/datetime.c
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 #include <stdio.h>
 #include <time.h>
-
+#include <string.h>
 #include "../slstatus.h"
 #include "../util.h"
 
@@ -9,12 +9,17 @@ const char *
 datetime(const char *fmt)
 {
 	time_t t;
-
+	char time_str[64];
+	static char result[128];  // Increased buffer size to accommodate icon and time
+	
 	t = time(NULL);
-	if (!strftime(buf, sizeof(buf), fmt, localtime(&t))) {
+	if (!strftime(time_str, sizeof(time_str), fmt, localtime(&t))) {
 		warn("strftime: Result string exceeds buffer size");
 		return NULL;
 	}
-
-	return buf;
+	
+	// Combine icon and time string
+	snprintf(result, sizeof(result), "󰥔 %s", time_str);
+	
+	return result;
 }
--- config.def.h
+++ config.def.h
@@ -65,6 +65,6 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-  { battery_perc, "%s ", "BAT0" },
+  { battery_perc, "^fg(ffff00)%s^fg() ", "BAT0" },
 	{ datetime, "%s ",     "%T" },
 };
